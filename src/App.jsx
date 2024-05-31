d33d87bde437df70d410e399f02ce290b973f404
dipu-37 <razibul1105nstu@gmail.com>
2024-05-31 09:56:07 +0600

    button click and parameter pass

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,15 @@ function App() {
     alert('button click')
   }
 
+  const handleclick2 = (num)=> {
+    alert(4 +num)
+  }
   return (
     <>
      
       <h2>React component part 2</h2>
     <button onClick={handleclick}>Click me</button>
+    <button onClick={ () => handleclick2(5)}>click me bro</button>
       
     </>
   )
